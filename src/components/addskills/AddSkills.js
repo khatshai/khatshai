788af76e5f45e1ad8cde00f05974d97f1438883f
kubra shaik <khati.shaik@capgemini.com>
2024-05-14 11:53:37 +0530

    added minor change

--- src/components/addskills/AddSkills.js
+++ src/components/addskills/AddSkills.js
@@ -2,24 +2,24 @@ import React, { useState } from "react";
 import { postSkills } from "../../services/RMPortalServices";
 
 function AddSkills() {
-  const [skill, setSkill] = useState("");
+  const [skill, setSkills] = useState("");
 
   const [sucessMsg, setSucessMsg] = useState(null);
-  const [error,setError] = useState(null);
+  const [error, setError] = useState(null);
 
   const onSkillsChange = (e) => {
-    setSkill(e.target.value);
+    setSkills(e.target.value);
   };
 
   const onHandleSkill = async () => {
     const post = await postSkills({ newSkill: skill });
     console.log(post);
-    if(post.sucess){
-      setSkill('');
+    if (post.sucess) {
+      setSkills("");
       setSucessMsg(post.msg);
-      setError('');
-    }else{
-      setSucessMsg('');
+      setError("");
+    } else {
+      setSucessMsg("");
       setError(post.msg);
     }
   };
@@ -27,28 +27,36 @@ function AddSkills() {
   return (
     <div className="addEmployeeContainer">
       <div className="formContainer">
-      <p className="sucess">{sucessMsg}</p>
-      <p className="error">{error}</p>
+        <p className="sucess">{sucessMsg}</p>
+        <p className="error">{error}</p>
         <h2 className="skillText">Add a new Skill</h2>
-        <p className="skillText">If you don't find your skills, add your skills here!</p>
+        <p className="skillText">
+          If you don't find your skills, add your skills here!
+        </p>
         <p className="skillText">Add one skill at a time.</p>
         <div className="addSkillContainer">
-        <label>Skill Name*</label>
-        <input
-        className="addSkillInput"
-          type="text"
-          placeholder="Enter skill name"
-          value={skill}
-          onChange={onSkillsChange}
-        />
-        <div className="submitButtonContainer addSkillButton">
-        <button className="submitButton" onClick={onHandleSkill}>Add Skill</button>
+          <label>Skill Name*</label>
+          <input
+            className="addSkillInput"
+            type="text"
+            placeholder="Enter skill name"
+            value={skill}
+            onChange={onSkillsChange}
+          />
+          <div className="submitButtonContainer addSkillButton">
+            <button className="submitButton" onClick={onHandleSkill}>
+              Add Skill
+            </button>
           </div>
         </div>
       </div>
 
       <div>
-        <img className="marginRight" src="/RMtoolSVGs/AddSkillSVG.svg" alt="AddSkill" />
+        <img
+          className="marginRight"
+          src="/RMtoolSVGs/AddSkillSVG.svg"
+          alt="AddSkill"
+        />
       </div>
     </div>
   );
